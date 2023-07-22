212e303a7142bb45d73b26b12a5661469e75ba01
Xiyan <xiyan128@gmail.com>
2023-07-22 15:13:01 -0400

    feat,refactor: visualizer utils and schema

--- rap/visualization/__init__.py
+++ rap/visualization/__init__.py
@@ -1,1 +1,33 @@
-from .tree import *
+from .tree_log import *
+from .visualizer_client import VisualizerClient
+
+
+class ReasonersVisualizer:
+    @staticmethod
+    def visualize(result: Union[TreeLog, MCTSResult]):
+
+        tree_log: TreeLog
+
+        if isinstance(result, TreeLog):
+            tree_log = result
+        elif isinstance(result, MCTSResult):
+            tree_log = TreeLog.from_mcts_results(result)
+        elif isinstance(result, ...):
+            raise NotImplementedError()
+        else:
+            raise TypeError(f"Unsupported result type: {type(result)}")
+
+        receipt = VisualizerClient().post_log(tree_log)
+
+        if receipt is None:
+            return
+
+        return ReasonersVisualizer._present_visualizer(receipt)
+
+    @staticmethod
+    def _present_visualizer(receipt: VisualizerClient.TreeLogReceipt):
+
+        import webbrowser
+        print(f"Visualizer URL: {receipt.access_url()}")
+
+        webbrowser.open(receipt.access_url())
---
+++ rap/visualization/visualizer_client.py
@@ -0,0 +1,51 @@
+import dataclasses
+import json
+from typing import Optional
+
+import requests
+
+from rap.visualization import TreeLog, TreeSnapshot
+
+_API_DEFAULT_BASE_URL = "https://2wz3t0av30.execute-api.us-west-1.amazonaws.com/staging"
+_VISUALIZER_DEFAULT_BASE_URL = "https://www.llm-reasoners.net"
+
+
+class VisualizerClient:
+    def __init__(self, base_url: str = _API_DEFAULT_BASE_URL) -> None:
+        self.base_url = base_url
+
+    @dataclasses.dataclass
+    class TreeLogReceipt:
+        id: str
+        access_key: str
+
+        def access_url(self) -> str:
+            return f"{_VISUALIZER_DEFAULT_BASE_URL}/visualizer/{self.id}?accessKey={self.access_key}"
+
+    class _TreeLogEncoder(json.JSONEncoder):
+        def default(self, o):
+            from numpy import float32
+
+            if isinstance(o, TreeSnapshot.Node):
+                return o.__dict__
+            elif isinstance(o, TreeSnapshot.Edge):
+                return o.__dict__
+            elif isinstance(o, TreeSnapshot):
+                return o.__dict__()
+            elif isinstance(o, float32):
+                return float(o)
+            if isinstance(o, TreeLog):
+                return {"logs": list(o)}
+            else:
+                return super().default(o)
+
+    def post_log(self, data: TreeLog) -> Optional[TreeLogReceipt]:
+        url = f"{self.base_url}/logs"
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post(url, headers=headers, data=json.dumps(data, cls=self._TreeLogEncoder))
+
+        if response.status_code != 200:
+            print(f"POST Log failed with status code: {response.status_code}, message: {response.text}")
+            return None
+
+        return self.TreeLogReceipt(**response.json())
