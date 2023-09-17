fff82dfe2807125ba819e9a3c64884815f41c74a
Haotian Luo <aegeanyanlht@gmail.com>
2023-09-17 18:54:12 +0000

    revert Nonetype address

--- examples/AQuA/inference.py
+++ examples/AQuA/inference.py
@@ -92,13 +92,11 @@ def rap_AQuA(base_model: LanguageModel,
     correct_count = 0
     for i, example in enumerate(tqdm(dataset, total=resume + len(dataset), initial=resume,
                                      desc='AQuA_clean', disable=disable_tqdm)):
-        flag = True
         try:
             algo_output = reasoner(example["question"])
         except AssertionError as e:
             from reasoners.algorithm import MCTSResult
             algo_output = MCTSResult(terminal_state=None, cum_reward=None, trace=None, trace_of_nodes=None, tree_state=None)
-            flag = False
         if algo_output.terminal_state is None:
             output = None
         else:
@@ -116,8 +114,6 @@ def rap_AQuA(base_model: LanguageModel,
                 print(log_str, file=f)
             with open(os.path.join(log_dir, 'algo_output', f'{resume + i + 1}.pkl'), 'wb') as f:
                 pickle.dump(algo_output, f)
-            if flag == False:
-                return
             if isinstance(search_algo, MCTS):
                 with open(os.path.join(log_dir, 'algo_output', f'{resume + i + 1}.json'), 'w') as f:
                     # noinspection PyTypeChecker
