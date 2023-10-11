b6e1f3c4240d00128f8227397e2b1f6bfc9dbc09
Haotian Luo <aegeanyanlht@gmail.com>
2023-10-11 19:41:42 +0000

    fix other files

--- examples/AQuA/utils.py
+++ examples/AQuA/utils.py
@@ -1,30 +1,24 @@
 import re
-from typing import Optional
+from typing import Optional, Union
 
 
-def retrieve_answer(output: str) -> Optional[str]:
+def retrieve_answer(output: Union[list, str]) -> Optional[str]:
+    '''
+    output should be a world_model.GSM8kState if being a list
+    '''
     if isinstance(output, list):
         output = output[-1].sub_answer
-    match = re.match(r'.*The answer is .*?([A-E]).*?\.$', output, re.DOTALL)
+    match = re.match(r'.*[Tt]he answer is.*?([A-E]).*?\.$', output, re.DOTALL)
     if match is None:
         return None
-    # answer = match[1].replace(',', '').replace('$', '').replace(' ', '')
-    # if '=' in answer:
-    #     answer = answer[answer.rindex('=') + 1:]
-    answer = match[1]
+    answer = match[1].replace(',', '').replace('$', '').replace(' ', '')
+    if '=' in answer:
+        answer = answer[answer.rindex('=') + 1:]
     return answer
 
-# def retrieve_answer(output: str) -> Optional[str]:
-#     match = re.search(r'.*The answer is .*?([ $.0-9,\-]+).*\.$', output)
-#     if match is None:
-#         return None
-#     answer = match[1].replace(',', '').replace('$', '').replace(' ', '')
-#     if '=' in answer:
-#         answer = answer[answer.rindex('=') + 1:]
-#     return answer
-    
+
 def retrieve_answer_from_dataset(answer: str) -> str:
-    return answer
+    return re.match(r'[\S\s]*#### (.*)$', answer)[1]
 
 
 def judge_answer(output: Optional[str], answer: str) -> bool:
@@ -43,3 +37,16 @@ def judge_answer(output: Optional[str], answer: str) -> bool:
     except ValueError:
         pass
     return output == answer
+
+def rap_extractor(algo_output, aggregate=True):
+    
+    from reasoners.algorithm import MCTSAggregation
+    if aggregate:
+        aggregator = MCTSAggregation(retrieve_answer, weight_policy='edge_inverse_depth')
+        output = aggregator(algo_output.tree_state)
+    else:
+        if algo_output.terminal_state is None:
+            output = None
+        else:
+            output = retrieve_answer(algo_output.terminal_state)
+    return output
