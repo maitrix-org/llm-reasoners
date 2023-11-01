d4c6f98a80ca0880c874a16bef3929df120536d9
Ber666 <ber66666@126.com>
2023-10-31 17:05:30 -0700

    update path and readme

--- examples/README.md
+++ examples/README.md
@@ -57,9 +57,13 @@ python examples/guided_gsm8k/inference.py --n_actions 16 --temperature 1 --rewar
     git checkout bf00a2196e92422d1000abc37dd050ef8186f2ab
     ```
 2. Set up `Val` for validation
-   1. Download from [here](https://www.fast-downward.org/SettingUpVal)
+   1. Install `Val` following this [link](https://github.com/karthikv792/LLMs-Planning/tree/34e6841f81ca7708f2f8b8241504bfe8a908e40b/planner_tools/VAL). Ideally you can directly use the executable files in this directory, and there is no need to build the tool yourself.
    2. Assign path of the folder to the environment variable VAL `export VAL=/path/to/val`
+
 ### RAP
+
+> Note: The commands below might be outdated. Please look at the scripts under `blocksworld` for updated commands.
+
 ```bash
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run --nproc_per_node 4 examples/rap_blocksworld/inference.py --llama_size "30B" --data_path 'examples/rap_blocksworld/data/step_4.json' --depth_limit 4 --output_trace_in_each_iter
 ```
--- examples/blocksworld/cot_inference.py
+++ examples/blocksworld/cot_inference.py
@@ -1,7 +1,6 @@
 from reasoners.lm import ExLlamaModel
 import json
 from reasoners.benchmark import BWEvaluator
-import utils
 import fire
 
 class CoTReasoner():
@@ -18,17 +17,16 @@ class CoTReasoner():
                                           eos_token_id='\n[').text[0][:-1].strip()
         return output
 
-def main(exllama_model_dir, exllama_lora_dir, exllama_mem_map, data_path, prompt_path, disable_log=False, batch_size=1, config_file: str = "examples/rap_blocksworld/data/bw_config.yaml", domain_file: str = "examples/rap_blocksworld/data/generated_domain.pddl", resume=0, log_dir=None):
+def main(exllama_model_dir, exllama_lora_dir, data_path, prompt_path, disable_log=False, batch_size=1, config_file: str = "examples/blocksworld/data/bw_config.yaml", domain_file: str = "examples/blocksworld/data/generated_domain.pddl", resume=0, log_dir=None, temperature=0.8, exllama_mem_map: str = None):
 
     base_model = ExLlamaModel(exllama_model_dir, exllama_lora_dir,
                           mem_map=exllama_mem_map, max_batch_size=batch_size,
                           max_new_tokens=300, max_seq_length=2048)
 
-
     with open(prompt_path) as f:
         prompt = json.load(f)
 
-    reasoner = CoTReasoner(base_model)
+    reasoner = CoTReasoner(base_model, temperature=temperature)
     evaluator = BWEvaluator(config_file=config_file, domain_file=domain_file, data_path=data_path, init_prompt=prompt, disable_log=disable_log, output_extractor=lambda x:x, sample_prompt_type="rap") # rap prompt includes cot
     accuracy = evaluator.evaluate(reasoner, shuffle_prompt=True, num_shot=4, resume=resume, log_dir=log_dir)
     print(f'accuracy: {accuracy:.4f}')
