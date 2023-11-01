3c40d5327c141c2354d20786cf2037a23a80abfa
Haotian Luo <aegeanyanlht@gmail.com>
2023-11-01 06:44:57 +0000

    add answerlist to visualize

--- examples/AQuA_rap/visualize.py
+++ examples/AQuA_rap/visualize.py
@@ -5,10 +5,10 @@ import os
 from reasoners.visualization import visualize
 from reasoners.visualization.tree_snapshot import NodeData
 from reasoners.algorithm.mcts import MCTSNode
-mcts_result = pickle.load(open('/data/haotian/RAP_tune/llm-reasoners/logs/aqua_MCTS/10202023-063340(1)/algo_output/1.pkl', 'rb'))
+mcts_result = pickle.load(open('/data/haotian/RAP_tune/llm-reasoners/logs/AQuA_clean_MCTS/10202023-063340/algo_output/5.pkl', 'rb'))
 # for child in mcts_result.tree_state.children:
 #     print(child.state[-1].answer_list)
-print(mcts_result.terminal_state[-1].answer_list)
+print(mcts_result.terminal_state[-2].answer_list)
 def gsm_node_data_factory(x: MCTSNode):
     if not x.state:
         return {}
