575ced16b8d7a161ffd86b44ef1681baed001423
Haotian Luo <aegeanyanlht@gmail.com>
2023-11-27 08:30:21 +0000

    make RAP-sc

--- examples/AQuA_rap/inference.py
+++ examples/AQuA_rap/inference.py
@@ -152,7 +152,76 @@ def rap_AQuA(base_model: LanguageModel,
     accuracy = evaluator.evaluate(reasoner, num_shot=4, resume=resume, log_dir=log_dir)
     print(f'accuracy: {accuracy:.4f}')
     return 0
-
+def rap_AQuA_sc(base_model: LanguageModel,
+              prompt: dict,
+              useful_prompt: dict,
+              search_algo: Type[SearchAlgorithm] = MCTS,
+              resume: int = 0,
+              n_action: int = 1,
+              n_confidence: int = 8,
+              depth_limit: int = 5,
+              force_terminating_on_depth_limit: bool = True,
+              batch_size: int = 1,
+              temperature: float = 0.8,
+              early_stop_base: int = 2,
+              early_stop_threshold: float = 0.5,
+              reward_alpha: float = 0.5,
+              reward_confidence_default: float = 0.8,
+              cum_reward: Callable[[list[float]], float] = np.mean,
+              calc_q: Callable[[list[float]], float] = max,
+              log_dir: Optional[str] = None,
+              disable_log: bool = False,
+              disable_tqdm: bool = False,
+              output_trace_in_each_iter: bool = True,
+              aggregate: bool = False,
+              **search_algo_params):
+    
+    print(f'aggregate: {aggregate}')
+    if aggregate:
+        aggregator = MCTSAggregation(utils.retrieve_answer, weight_policy='edge')
+    else:
+        aggregator = None
+    
+    search_algo_params |= {'cum_reward': cum_reward, 
+                           'calc_q': calc_q, 
+                           'disable_tqdm': disable_tqdm, 
+                           'output_trace_in_each_iter': output_trace_in_each_iter,
+                           'node_visualizer': node_visualizer, 
+                           'aggregator': aggregator,
+                           'w_exp': 1.0,
+                           }
+    
+    world_model = MATHWorldModel(
+        base_model=base_model,
+        n_confidence=n_confidence, 
+        batch_size=batch_size, 
+        temperature=temperature,
+        early_stop_base=early_stop_base, 
+        early_stop_threshold=early_stop_threshold)
+    
+    config = MATHConfig(
+        base_model=base_model, 
+        useful_prompt=useful_prompt,
+        n_actions=n_action, 
+        batch_size=batch_size, 
+        temperature=temperature,
+        reward_alpha=reward_alpha, 
+        reward_confidence_default=reward_confidence_default,
+        force_terminating_on_depth_limit=force_terminating_on_depth_limit, 
+        depth_limit=depth_limit)
+    
+    search_algo = search_algo(**search_algo_params)
+    
+    reasoner = Reasoner(world_model=world_model, search_config=config, search_algo=search_algo)
+    evaluator = AQuAEvaluator(output_extractor=utils.retrieve_answer,
+                               answer_extractor=lambda x: utils.retrieve_answer_from_dataset(x["answer"]),
+                               init_prompt=prompt,
+                               sample_prompt_type="rap",
+                               disable_log=disable_log,
+                               disable_tqdm=disable_tqdm)
+    accuracy = evaluator.evaluate_sc(reasoner, num_shot=4, resume=resume, log_dir=log_dir)
+    print(f'accuracy: {accuracy:.4f}')
+    return 0
 if __name__ == '__main__':
     import os
     import sys
--- reasoners/base.py
+++ reasoners/base.py
@@ -220,6 +220,76 @@ class Evaluator():
         
         return accuracy
 
+    def evaluate_sc(self,
+                 reasoner,
+                 shuffle_prompt=True,
+                 num_shot=4,
+                 resume=0,
+                 n_sc = 10,
+                 log_dir=None):
+
+        self.dataset = list(self.full_dataset)[resume:]
+        try:
+            algo_name = reasoner.search_algo.__class__.__name__
+        except:
+            algo_name = "unknown"
+
+        
+        if not torch.distributed.is_initialized() or torch.distributed.get_rank() == 0:
+            if log_dir is None:
+                log_dir = f'logs/{self._dataset_name}_'\
+                        f'{algo_name}/'\
+                        f'{datetime.now().strftime("%m%d%Y-%H%M%S")}'
+            os.makedirs(log_dir, exist_ok=resume > 0)
+            os.makedirs(os.path.join(log_dir, 'algo_output'), exist_ok=True)
+        
+            with open(os.path.join(log_dir, 'args.txt'), 'w') as f:
+                print(sys.argv, file=f)
+
+        correct_count = 0
+
+        disable_tqdm = self.disable_tqdm or \
+            (torch.distributed.is_initialized() and torch.distributed.get_rank() != 0)
+        for i, example in enumerate(tqdm(self.dataset,
+                                            total=resume + len(self.dataset),
+                                            initial=resume,
+                                            desc=self._dataset_name,
+                                            disable=self.disable_tqdm)):
+            
+            prompt = self.sample_prompt(
+                            shuffle_prompt=shuffle_prompt,
+                            num_shot=num_shot,
+                            sample_prompt_type=self.sample_prompt_type)
+            output_list = []
+            save_list = []
+            for j in range(n_sc):
+                algo_output = reasoner(self.input_processor(example),
+                                    prompt=prompt)
+                terminal_state = algo_output.terminal_state
+                path = ""
+                for k in range(len(terminal_state)):
+                    path += terminal_state[k].sub_question + " " + terminal_state[k].sub_answer + " "
+                save_list.append(path)
+                output = self.output_extractor(algo_output)
+                output_list.append(output)
+                answer = self.answer_extractor(example)
+            from collections import Counter
+            output = Counter(output_list).most_common(1)[0][0]
+            correct = self.eval_output(answer, output)
+            correct_count += correct
+            accuracy = correct_count / (i + 1)
+            log_str = f'Case #{resume + i + 1}: {correct=}, {output=}, {answer=};'\
+                        f'{accuracy=:.3f} ({correct_count}/{i + 1})'
+            tqdm.write(log_str)
+
+            if (not self.disable_log) and \
+                (not torch.distributed.is_initialized() or torch.distributed.get_rank() == 0):
+                with open(os.path.join(log_dir, 'result.log'), 'a') as f:
+                    print(log_str, file=f)
+                with open(os.path.join(log_dir, 'algo_output.txt'),'a') as f1:
+                    print(save_list, file=f1)
+        
+        return accuracy
     @abstractmethod
     def eval_output(self, answer, output):
         pass
