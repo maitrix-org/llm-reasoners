f808dbf367428ffd08d92982a0d5ba2737610c07
Haotian Luo <aegeanyanlht@gmail.com>
2023-12-02 21:15:31 +0000

    visualize

--- examples/AQuA_rap/visualize.py
+++ examples/AQuA_rap/visualize.py
@@ -5,11 +5,16 @@ import os
 from reasoners.visualization import visualize
 from reasoners.visualization.tree_snapshot import NodeData
 from reasoners.algorithm.mcts import MCTSNode
-mcts_result = pickle.load(open('/data/haotian/RAP_tune/llm-reasoners/logs/AQuA_clean_MCTS/10312023-202328/algo_output/1.pkl', 'rb'))
-# for child in mcts_result.tree_state.children:
-#     print(child.state[-1].answer_list)
-# print(mcts_result.terminal_state[-2].answer_list)
-print(mcts_result.aggregated_result)
+mcts_result = pickle.load(open('/data/haotian/RAP_tune/llm-reasoners/logs/AQuA_clean_MCTS/12012023-193559/algo_output/7.pkl', 'rb'))
+print(mcts_result.terminal_state[-1].answer_list)
+print(mcts_result.terminal_state[-1].answer_values)
+print(len(mcts_result.terminal_state[-1].answer_list))
+dict_values = mcts_result.terminal_state[-1].answer_values
+length = 0
+for i in dict_values:
+    length += len(i)
+print(length)
+# print(mcts_result.aggregated_result)
 def gsm_node_data_factory(x: MCTSNode):
     if not x.state:
         return {}
