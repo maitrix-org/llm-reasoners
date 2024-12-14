7328c14ad61d187414982cdae28a1f8a330f6713
BlankCheng <913501223@qq.com>
2024-12-14 15:28:33 -0800

    Update visulizer to support verbose/simple state and node info

--- examples/browsergym/visualize.py
+++ examples/browsergym/visualize.py
@@ -1,23 +1,85 @@
 import argparse
 import pickle
-
+import numpy as np
 from reasoners.visualization import visualize
 from reasoners.visualization.tree_snapshot import NodeData, EdgeData
 from reasoners.algorithm.mcts import MCTSNode
 from browsergym.core.action.parsers import highlevel_action_parser
-
 from examples.browsergym.gym_env import StateGym
 
 
-def browsergym_node_data_factory(n: MCTSNode) -> NodeData:
-    return NodeData(
-        {"expanded": "expanded"}
-        if n.state is not None
-        else {"expanded": "not expanded"}
+def process_obs_for_viz(obs: dict[str, any], verbose: bool = False):
+    """Process the observation for visualization"""
+    processed_obs = {}
+
+    # Convert tuple/array fields (keeping the iteration as needed)
+    for k, v in obs.items():
+        if isinstance(v, tuple) or isinstance(v, np.ndarray):
+            obs[k] = list(v)
+        processed_obs[k] = v
+
+    # Truncate the long text fields to 50 characters
+    processed_obs.update(
+        {k: str(obs[k])[:50] for k in ["axtree_txt", "pruned_html"] if k in obs}
     )
+    # Convert int64 active_page_index to int to be serialized
+    if "active_page_index" in obs:
+        processed_obs["active_page_index"] = [int(x) for x in obs["active_page_index"]]
+    # Extract clean action history from the whole action history string
+    if "action_history" in obs:
+        processed_obs["clean_action_history"] = list(
+            map(simple_parse_action_from_proposal_string, obs["action_history"])
+        )
+    # Extract clean action from the last action string
+    if "last_action" in obs:
+        processed_obs["clean_last_action"] = simple_parse_action_from_proposal_string(
+            obs["last_action"]
+        )
+
+    # FIXME: hardcode to stringfy screenshot as it's too large for server upload; remove this line if the server supports large file upload
+    processed_obs["screenshot"] = str(processed_obs["screenshot"])[:50]
+
+    if not verbose:
+        return {
+            "screenshot": processed_obs["screenshot"],
+            "last_action": processed_obs["clean_last_action"],
+        }
+
+    return processed_obs
 
 
-def browsergym_edge_data_factory(n: MCTSNode) -> EdgeData:
+def simple_parse_action_from_proposal_string(proposal: str):
+    """Extract the action from the proposal string wrapped in triple backticks"""
+    import re
+
+    match = re.search(r"```(.+?)```", proposal)
+    return match.group(1) if match else proposal
+
+
+def browsergym_node_data_factory(x: MCTSNode, verbose: bool = False):
+    """Generate the node data for the tree visualization"""
+    if not x.state:
+        return {}
+    current_obs = process_obs_for_viz(x.state.current_obs, verbose)
+
+    if not verbose:
+        return {
+            "step_idx": int(x.state.step_idx),
+            "reward": x.state.reward,
+            **current_obs,
+        }
+    else:
+        return {
+            "step_idx": int(x.state.step_idx),
+            "action_history": x.state.action_history,
+            "reward": x.state.reward,
+            "terminated": x.state.terminated,
+            "truncated": x.state.truncated,
+            **current_obs,
+        }
+
+
+def browsergym_edge_data_factory(n: MCTSNode, verbose: bool = False) -> EdgeData:
     function_calls = highlevel_action_parser.search_string(n.action)
     function_calls = sum(function_calls.as_list(), [])
 
@@ -39,13 +101,13 @@ def browsergym_edge_data_factory(n: MCTSNode) -> EdgeData:
     )
 
 
-def load_and_visualize(task_name: str):
-    result = pickle.load(open(f"./results/tree-search/{task_name}/result.pkl", "rb"))
+def load_and_visualize(args):
+    result = pickle.load(open(f"{args.input_dir}/{args.task_name}/result.pkl", "rb"))
 
     visualize(
         result,
-        node_data_factory=browsergym_node_data_factory,
-        edge_data_factory=browsergym_edge_data_factory,
+        node_data_factory=lambda x: browsergym_node_data_factory(x, args.verbose),
+        edge_data_factory=lambda x: browsergym_edge_data_factory(x, args.verbose),
     )
 
 
@@ -59,6 +121,18 @@ if __name__ == "__main__":
         required=True,
         help="The name of the task to visualize.",
     )
+    parser.add_argument(
+        "--input_dir",
+        type=str,
+        default="results/tree-search",
+        help="The directory to save the visualization results.",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="Whether to print the visualization results.",
+    )
     args = parser.parse_args()
 
-    load_and_visualize(args.task_name)
+    load_and_visualize(args)
