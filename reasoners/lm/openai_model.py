b9952290222320138c2dca1574ef7342a9afd47b
BlankCheng <913501223@qq.com>
2024-12-14 14:16:15 -0800

    Add backend switch (sglang)

--- reasoners/lm/openai_model.py
+++ reasoners/lm/openai_model.py
@@ -4,39 +4,64 @@ import numpy as np
 from typing import Optional, Union, Literal
 import time
 
-from .. import LanguageModel, GenerateOutput
+from reasoners.base import LanguageModel, GenerateOutput
 from openai import OpenAI
 
-PROMPT_TEMPLATE_ANSWER = "Your response need to be ended with \"So the answer is\"\n\n"
+PROMPT_TEMPLATE_ANSWER = 'Your response need to be ended with "So the answer is"\n\n'
 PROMPT_TEMPLATE_CONTINUE = "Please continue to answer the last question, following the format of previous examples. Don't say any other words.\n\n"
 
+
 class OpenAIModel(LanguageModel):
-    def __init__(self, model:str, max_tokens:int = 2048, temperature=0.0, additional_prompt=None):
+    def __init__(
+        self,
+        model: str,
+        max_tokens: int = 2048,
+        temperature=0.0,
+        additional_prompt=None,
+        backend: Literal["openai", "sglang"] = "openai",
+        is_instruct_model: bool = False,
+    ):
         self.model = model
         self.max_tokens = max_tokens
         self.temperature = temperature
-        self.client = OpenAI(
-            api_key = os.getenv("OPENAI_API_KEY", None),
-            # organization='',
-        )
+        self.backend = backend
         self.additional_prompt = additional_prompt
-    
-    def generate(self,
-                prompt: Optional[Union[str, list[str]]],
-                max_tokens: int = None,
-                top_p: float = 1.0,
-                num_return_sequences: int = 1,
-                rate_limit_per_min: Optional[int] = 20,
-                stop: Optional[str] = None,
-                logprobs: Optional[int] = None,
-                temperature = None,
-                additional_prompt=None,
-                retry = 64,
-                **kwargs) -> GenerateOutput:
-        
-        gpt_temperature = self.temperature if temperature is None else temperature
+        self.is_instruct_model = is_instruct_model
+        self.__init_client__()
+
+    def __init_client__(self):
+        if self.backend == "openai":
+            self.client = OpenAI(
+                api_key=os.getenv("OPENAI_API_KEY", None),
+            )
+        elif self.backend == "sglang":
+            self.client = OpenAI(
+                base_url=os.getenv("SGLANG_API_URL", None),
+            )
+        else:
+            raise ValueError(f"Invalid backend: {self.backend}")
+
+    def generate(
+        self,
+        prompt: Optional[Union[str, list[str]]],
+        max_tokens: int = None,
+        top_p: float = 1.0,
+        num_return_sequences: int = 1,
+        rate_limit_per_min: Optional[int] = 20,
+        stop: Optional[str] = None,
+        logprobs: Optional[int] = None,
+        temperature=None,
+        additional_prompt=None,
+        retry=64,
+        **kwargs,
+    ) -> GenerateOutput:
+
+        max_tokens = self.max_tokens if max_tokens is None else max_tokens
+        temperature = self.temperature if temperature is None else temperature
+        logprobs = 0 if logprobs is None else logprobs
+
         if isinstance(prompt, list):
-            assert len(prompt) == 1
+            assert len(prompt) == 1  # @zj: why can't we pass a list of prompts?
             prompt = prompt[0]
         if additional_prompt is None and self.additional_prompt is not None:
             additional_prompt = self.additional_prompt
@@ -47,12 +72,16 @@ class OpenAIModel(LanguageModel):
         elif additional_prompt == "CONTINUE":
             prompt = PROMPT_TEMPLATE_CONTINUE + prompt
 
-        if max_tokens is None:
-            max_tokens = self.max_tokens
-        
-        if logprobs is None:
-            logprobs = 0
-
+        is_instruct_model = self.is_instruct_model
+        if not is_instruct_model:
+            # Recheck if the model is an instruct model with model name
+            model_name = self.model.lower()
+            if (
+                ("gpt-3.5" in model_name)
+                or ("gpt-4" in model_name)
+                or ("instruct" in model_name)
+            ):
+                is_instruct_model = True
 
         for i in range(1, retry + 1):
             try:
@@ -60,7 +89,7 @@ class OpenAIModel(LanguageModel):
                 if rate_limit_per_min is not None:
                     time.sleep(60 / rate_limit_per_min)
                 ### GPT 3.5 and higher use a different API
-                if ('gpt-3.5' in self.model) or ('gpt-4' in self.model):
+                if is_instruct_model:
                     messages = [{"role": "user", "content": prompt}]
                     response = self.client.chat.completions.create(
                         model=self.model,
@@ -69,11 +98,11 @@ class OpenAIModel(LanguageModel):
                         temperature=temperature,
                         top_p=top_p,
                         n=num_return_sequences,
-                        stop=stop
+                        stop=stop,
                     )
                     return GenerateOutput(
                         text=[choice.message.content for choice in response.choices],
-                        log_prob=None
+                        log_prob=None,
                     )
                 else:
                     response = self.client.chat.completions.create(
@@ -85,32 +114,46 @@ class OpenAIModel(LanguageModel):
                         n=num_return_sequences,
                         stop=stop,
                         logprobs=0,
-                        **kwargs
+                        **kwargs,
                     )
                     return GenerateOutput(
                         text=[choice["text"] for choice in response.choices],
-                        log_prob=[choice["logprobs"] for choice in response["choices"]]
+                        log_prob=[choice["logprobs"] for choice in response["choices"]],
                     )
-            
+
             except Exception as e:
                 print(f"An Error Occured: {e}, sleeping for {i} seconds")
                 time.sleep(i)
-        
-        # after 64 tries, still no luck
-        raise RuntimeError("GPTCompletionModel failed to generate output, even after 64 tries")
-    
-    def get_next_token_logits(self,
-                              prompt: Union[str, list[str]],
-                              candidates: Union[list[str], list[list[str]]],
-                              **kwargs) -> list[np.ndarray]:
-        raise NotImplementedError("GPTCompletionModel does not support get_next_token_logits")
 
-    def get_loglikelihood(self,
-                          prompt: Union[str, list[str]],
-                          **kwargs) -> list[np.ndarray]:
+        # after 64 tries, still no luck
+        raise RuntimeError(
+            "GPTCompletionModel failed to generate output, even after 64 tries"
+        )
+
+    def get_next_token_logits(
+        self,
+        prompt: Union[str, list[str]],
+        candidates: Union[list[str], list[list[str]]],
+        **kwargs,
+    ) -> list[np.ndarray]:
+        raise NotImplementedError(
+            "GPTCompletionModel does not support get_next_token_logits"
+        )
+
+    def get_loglikelihood(
+        self, prompt: Union[str, list[str]], **kwargs
+    ) -> list[np.ndarray]:
         raise NotImplementedError("GPTCompletionModel does not support get_log_prob")
 
 
-if __name__ == '__main__':
-    model = OpenAIModel(model='gpt-3.5-turbo')
-    print(model.generate(['Hello, how are you?', 'How to go to Shanghai from Beijing?']))
+if __name__ == "__main__":
+    model = OpenAIModel(model="gpt-3.5-turbo")
+    print("-------OpenAI client-------")
+    print(model.generate(["How to go to Shanghai from Beijing?"]))
+    print("-------SGLang client-------")
+    model = OpenAIModel(
+        model="meta-llama/Llama-3.1-8B-Instruct",
+        backend="sglang",
+        is_instruct_model=True,
+    )
+    print(model.generate(["How to go to Shanghai from Beijing?"]))
