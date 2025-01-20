15aaea9f29508b790a07b3f74a97df935aa23f2a
0eti <samuelzhang012@gmail.com>
2025-01-19 16:07:54 -0800

    focusing on mcts occam

---
+++ examples/browsergym/occam_agent/__init__.py
@@ -0,0 +1,13 @@
+from .agent_configs import (
+    AGENT_4o,
+    AGENT_4o_MINI,
+    AGENT_4o_VISION,
+    AGENT_DEEPSEEK,
+)
+
+__all__ = [
+    "AGENT_4o",
+    "AGENT_4o_MINI",
+    "AGENT_4o_VISION",
+    "AGENT_DEEPSEEK",
+]
