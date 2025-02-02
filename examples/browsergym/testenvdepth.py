700b92c2f03501fb489649e9a28d04536523849e
0eti <samuelzhang012@gmail.com>
2025-02-02 13:10:55 -0800

    found the culprit for the depth issue. llm reasoners has a redundant check which completely overlaps with truncated.

--- examples/browsergym/testenvdepth.py
+++ examples/browsergym/testenvdepth.py
@@ -1,3 +1,30 @@
+# there seems to be an issue where the experiments cannot go past depth 20 even if the env max_depth is set to 100. the max_depth parameter evidently should work as when testing with max_depth 1, it is controlled properly.
+
+# this file serves as a sanity check to see if setting max depth to 100, then repeating a no_op action over and over would be able to reach the max depth. if not, then you know that something is fucking with you in browsergym. if not, then it's probably an issue w. mcts or the search configruation.
+
+# ok spamming click 42 doesn't seem to trigger the env shutdown. need to go in and look to see how exactly the env max step termination is being controlled.
+
+# ok. i'm stupid. it just flips the truncated flag to true. i completely forgot about this. let me see how i'm handling this in the search config.
+# def is_terminal(self, state: StateGym) -> bool:
+# return state.terminated or state.truncated or state.step_idx >= self.max_steps
+
+# so yeah. the search algorithm is being controlled by a check to see if it's a terminal node. one of these conditions is tripping at state 20. it's probably not state.truncated or state.terminated, as those as coming directly from the env itself, and this file's test suggests that the env is perfectly fine and working as expected. so it's probably state.step_idx >= self.max_steps?
+
+
+# FUUUUUUUUUUUUUUUUUUCK. YEP. THAT's exactly what it was. there's another max_steps being considered here. i'm a fucking idiot.
+#     def __init__(self, env: gym.Env, env_seed: int = 42, max_steps=20, obs_preprocessor: Optional[Callable[[dict], dict]] = None, task_dir: str = None):
+# self.env = env
+# self.env_seed = env_seed
+# self.obs_preprocessor = obs_preprocessor
+# self.max_steps = max_steps
+# self.env_current_obs: dict = None
+# self.task_dir = task_dir
+
+# why even is this check here. it's redundant with the browserenv max_episode_steps. it's functionality completely overlaps with truncated. i just made a bad assumption that it as doing something unique.
+
+# well this should be the issue found.
+
+
 import argparse
 import os
 import pickle
@@ -22,7 +49,7 @@ signal.signal(signal.SIGALRM, timeout_handler)
 
 depth = 100
 portion = 0
-name = f"d{depth}-{portion}"
+name = f"test_d{depth}-{portion}"
 
 
 def run_exp(exp_name: str, task_names: str):
@@ -69,8 +96,8 @@ def run_task(exp_name: str, task_name: str) -> bool:
     env_args = EnvArgs(
         task_name=task_name,
         task_seed=42,
-        # max_steps=depth,
-        max_steps=1,
+        max_steps=depth,
+        # max_steps=3,
         headless=False,
         record_video=True,
     )
@@ -83,41 +110,49 @@ def run_task(exp_name: str, task_name: str) -> bool:
         exp_dir=task_dir,
     )
 
-    llm = OpenAIModel(
-        model="gpt-4o-mini",
-        temperature=0.7,
-        task_dir=task_dir
-    )
+    env.reset()
+    print(env)
+    for idx in range(depth):
+        print(idx)
+        obs, reward, terminated, truncated, step_info = env.step(
+            "noop(10)\n")
+        print(terminated, truncated)
 
-    world_model = EnvironmentGym(
-        env=env, obs_preprocessor=obs_preprocessor, task_dir=task_dir)
+    # llm = OpenAIModel(
+    #     model="gpt-4o-mini",
+    #     temperature=0.7,
+    #     task_dir=task_dir
+    # )
 
-    # greedy search
-    search_config = SearchConfigBrowsergym(
-        action_set=browser_action_set,
-        n_proposals=10,
-        llm=llm,
-        use_axtree=True,
-        use_html=False,
-        use_screenshot=False,
-        task_dir=task_dir
-    )
-    algorithm = MCTS(  # no search happening here
-        n_iters=1,
-        depth_limit=depth,
-        w_exp=10**0.5,
-        uct_with_fast_reward=True,
-        disable_tqdm=False,
-        output_trace_in_each_iter=True,
-        task_dir=task_dir
-    )
+    # world_model = EnvironmentGym(
+    #     env=env, obs_preprocessor=obs_preprocessor, task_dir=task_dir)
 
-    reasoner = Reasoner(world_model, search_config, algorithm)
+    # # greedy search
+    # search_config = SearchConfigBrowsergym(
+    #     action_set=browser_action_set,
+    #     n_proposals=10,
+    #     llm=llm,
+    #     use_axtree=True,
+    #     use_html=False,
+    #     use_screenshot=False,
+    #     task_dir=task_dir
+    # )
+    # algorithm = MCTS(  # no search happening here
+    #     n_iters=1,
+    #     depth_limit=depth,
+    #     w_exp=10**0.5,
+    #     uct_with_fast_reward=True,
+    #     disable_tqdm=False,
+    #     output_trace_in_each_iter=True,
+    #     task_dir=task_dir
+    # )
 
-    plan_result = reasoner()
+    # reasoner = Reasoner(world_model, search_config, algorithm)
 
-    with open(f"{task_dir}/result.pkl", "wb") as f:
-        pickle.dump(plan_result, f)
+    # plan_result = reasoner()
+
+    # with open(f"{task_dir}/result.pkl", "wb") as f:
+    #     pickle.dump(plan_result, f)
 
     env.close()
 
@@ -126,7 +161,7 @@ def run_task(exp_name: str, task_name: str) -> bool:
     with open(f"{task_dir}/time.txt", "a+") as f:
         f.write(f"total time taken: {end - start}\n")
 
-    return plan_result.terminal_state and plan_result.terminal_state.reward == 1.0
+    return False
 
 
 tasks_0 = [
@@ -266,3 +301,208 @@ elif portion == 4:
     run_exp(name, tasks_4)
 elif portion == 5:
     run_exp(name, tasks_5)
+
+
+"""
+<TimeLimit<OrderEnforcing<BrowserEnv<browsergym/webarena.30>>>>
+0
+False False
+1
+False False
+2
+False False
+3
+False False
+4
+False False
+5
+False False
+6
+False False
+7
+False False
+8
+False False
+9
+False False
+10
+False False
+11
+False False
+12
+False False
+13
+False False
+14
+False False
+15
+False False
+16
+False False
+17
+False False
+18
+False False
+19
+False False
+20
+False False
+21
+False False
+22
+False False
+23
+False False
+24
+False False
+25
+False False
+26
+False False
+27
+False False
+28
+False False
+29
+False False
+30
+False False
+31
+False False
+32
+False False
+33
+False False
+34
+False False
+35
+False False
+36
+False False
+37
+False False
+38
+False False
+39
+False False
+40
+False False
+41
+False False
+42
+False False
+43
+False False
+44
+False False
+45
+False False
+46
+False False
+47
+False False
+48
+False False
+49
+False False
+50
+False False
+51
+False False
+52
+False False
+53
+False False
+54
+False False
+55
+False False
+56
+False False
+57
+False False
+58
+False False
+59
+False False
+60
+False False
+61
+False False
+62
+False False
+63
+False False
+64
+False False
+65
+False False
+66
+False False
+67
+False False
+68
+False False
+69
+False False
+70
+False False
+71
+False False
+72
+False False
+73
+False False
+74
+False False
+75
+False False
+76
+False False
+77
+False False
+78
+False False
+79
+False False
+80
+False False
+81
+False False
+82
+False False
+83
+False False
+84
+False False
+85
+False False
+86
+False False
+87
+False False
+88
+False False
+89
+False False
+90
+False False
+91
+False False
+92
+False False
+93
+False False
+94
+False False
+95
+False False
+96
+False False
+97
+False False
+98
+False False
+99
+False True
+"""
