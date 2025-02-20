3d854e04d9c01cf13922aaf7847c5fa73f4cfc8e
zzirving <ziz057@ucsd.edu>
2025-02-20 10:35:16 -0800

    merged reasoner.py into run.py

--- examples/LongCoT_Search/ProsQA/run.py
+++ examples/LongCoT_Search/ProsQA/run.py
@@ -1,10 +1,20 @@
 import os
 import argparse
 from utils import prosqa_extractor 
-from reasoner import ProsQAReasoner
 from reasoners.lm.deepseek_model import DeepseekModel
 from reasoners.benchmark.prosqa import ProsQAEvaluator
 
+class ProsQAReasoner():
+    def __init__(self, base_model,temperature=0, bs=1):
+        self.base_model = base_model
+        self.temperature = temperature
+        
+    def __call__(self, example, prompt=None):
+        outputs = self.base_model.generate(
+            [example], 
+            temeprature = self.temperature, 
+            hide_input=True).text[0]
+        return outputs
 
 def parse_args():
     parser = argparse.ArgumentParser(description='ProsQA Evaluation Script')
